10d96861b633f3e8f8c9cd9c4a1bdc9f6142a35c
benunah-mat <benunah@gmail.com>
2026-03-16 23:49:45 +0000

    damage_evolution_model.m

--- damage_evolution_model.m
+++ damage_evolution_model.m
@@ -1,14 +1,17 @@
 function D = damage_evolution_model(machine,t,dt,k,a,b,c,d)
 
+gamma = 3;   % damage acceleration factor
+
 D = zeros(size(t));
 
 for i = 2:length(t)
 
+base_growth = k*(machine.Load^a) * ...
+              (machine.Temperature^b) * ...
+              (machine.Speed^c) * ...
+              (machine.Lubrication^-d);
 
-growth = k*(machine.Load^a) * ...
-         (machine.Temperature^b) * ...
-         (machine.Speed^c) * ...
-         (machine.Lubrication^-d);
+growth = base_growth * (1 + gamma*D(i-1));
 
 D(i) = D(i-1) + growth*dt;
 
@@ -17,5 +20,6 @@ if D(i) >= 1
     break
 end
 
+end
 
 end
